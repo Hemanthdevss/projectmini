828266beb2100582a2b90dbccbe454ca9691068f
Hemanthdevss <Hemanth.devss@gmail.com>
2024-07-21 06:41:21 +0000

    ch

--- src/App.js
+++ src/App.js
@@ -1,148 +1,30 @@
 import './App.css'
+import {Switch, Route} from 'react-router-dom'
 
-const emojisList = [
-  {
-    id: 0,
-    emojiName: 'Face with stuck out tongue',
-    emojiUrl:
-      'https://assets.ccbp.in/frontend/react-js/face-with-stuck-out-tongue-img.png',
-  },
-  {
-    id: 1,
-    emojiName: 'Face with head bandage',
-    emojiUrl:
-      'https://assets.ccbp.in/frontend/react-js/face-with-head-bandage-img.png',
-  },
-  {
-    id: 2,
-    emojiName: 'Face with hugs',
-    emojiUrl: 'https://assets.ccbp.in/frontend/react-js/face-with-hugs-img.png',
-  },
-  {
-    id: 3,
-    emojiName: 'Face with laughing',
-    emojiUrl:
-      'https://assets.ccbp.in/frontend/react-js/face-with-laughing-img.png',
-  },
-  {
-    id: 4,
-    emojiName: 'Laughing face with hand in front of mouth',
-    emojiUrl:
-      'https://assets.ccbp.in/frontend/react-js/face-with-laughing-with-hand-infront-mouth-img.png',
-  },
-  {
-    id: 5,
-    emojiName: 'Face with mask',
-    emojiUrl: 'https://assets.ccbp.in/frontend/react-js/face-with-mask-img.png',
-  },
-  {
-    id: 6,
-    emojiName: 'Face with silence',
-    emojiUrl:
-      'https://assets.ccbp.in/frontend/react-js/face-with-silence-img.png',
-  },
-  {
-    id: 7,
-    emojiName: 'Face with stuck out tongue and winked eye',
-    emojiUrl:
-      'https://assets.ccbp.in/frontend/react-js/face-with-stuck-out-tongue-and-winking-eye-img.png',
-  },
-  {
-    id: 8,
-    emojiName: 'Grinning face with sweat',
-    emojiUrl:
-      'https://assets.ccbp.in/frontend/react-js/grinning-face-with-sweat-img.png',
-  },
-  {
-    id: 9,
-    emojiName: 'Smiling face with heart eyes',
-    emojiUrl:
-      'https://assets.ccbp.in/frontend/react-js/smiling-face-with-heart-eyes-img.png',
-  },
-  {
-    id: 10,
-    emojiName: 'Grinning face',
-    emojiUrl: 'https://assets.ccbp.in/frontend/react-js/grinning-face-img.png',
-  },
-  {
-    id: 11,
-    emojiName: 'Smiling face with star eyes',
-    emojiUrl:
-      'https://assets.ccbp.in/frontend/react-js/smiling-face-with-star-eyes-img.png',
-  },
-]
+import GamigHomeScreen from './components/GamingHomeScreen'
+import RPSgameHomePage from './components/RPSgameHomePage'
+import RPSgame from './components/RPSgame'
+import EmojiRules from './components/EmojiRules'
+import EmojiGame from './components/EmojiGame'
+import EmojiGameResult from './components/EmojiGameResultView'
+import CardFlipHomePage from './components/CardFlipHomePage'
+import CardFlipGame from './components/CardFlipGame'
+import MemoryMatrixHome from './components/MemoryMatrixHome'
+import MemoryGameBoard from './components/MemoryMatrixGameBoard'
 
-const choicesList = [
-  {
-    id: 'rock',
-    imageUrl:
-      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/rock-image.png',
-  },
-  {
-    id: 'scissor',
-    imageUrl:
-      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/scissor-image.png',
-  },
-  {
-    id: 'paper',
-    imageUrl:
-      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/paper-image.png',
-  },
-]
-
-const cardsData = [
-  {
-    name: 'tiger',
-    image:
-      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-tiger-img.png',
-  },
-  {
-    name: 'lion',
-    image:
-      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-lion-img.png',
-  },
-  {
-    name: 'rat',
-    image:
-      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-rat-img.png',
-  },
-  {
-    name: 'hen',
-    image:
-      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-hen-img.png',
-  },
-  {
-    name: 'elephant',
-    image:
-      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-elephant-img.png',
-  },
-  {
-    name: 'buffalo',
-    image:
-      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-buffalo-img.png',
-  },
-  {
-    name: 'goat',
-    image:
-      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-goat-img.png',
-  },
-  {
-    name: 'zebra',
-    image:
-      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-zebra-img.png',
-  },
-  {
-    name: 'duck',
-    image:
-      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-duck-img.png',
-  },
-  {
-    name: 'pigeon',
-    image:
-      'https://new-assets.ccbp.in/frontend/content/react-js/card-flip-memory/card-flip-memory-game-pigeon-img.png',
-  },
-]
-
-const App = () => <div>Hello World</div>
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={GamigHomeScreen} />
+    <Route exact path="/rpsGame" component={RPSgameHomePage} />
+    <Route exact path="/playRpsgame" component={RPSgame} />
+    <Route exact path="/emojiGame" component={EmojiRules} />
+    <Route exact path="/playEmojiGame" component={EmojiGame} />
+    <Route exact path="/emojiResult" component={EmojiGameResult} />
+    <Route exact path="/cardFlipGame" component={CardFlipHomePage} />
+    <Route exact path="/playCardflipGame" component={CardFlipGame} />
+    <Route exact path="/memoryMatrixGame" component={MemoryMatrixHome} />
+    <Route exact path="/playMemoryMatrixgame" component={MemoryGameBoard} />
+  </Switch>
+)
 
 export default App
